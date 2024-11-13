efaec8a6d53cfe23d5a9d2103c239c7f3c92f6fb
hohshho <tkdgur8377@gmail.com>
2024-11-13 23:35:51 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 126ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241113/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+    static int N, R, C;
+    static int[][] map;
+    // 상, 하, 좌, 우 
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        N = sc.nextInt();
+        R = sc.nextInt() - 1;
+        C = sc.nextInt() - 1;
+
+        map = new int[N][N];
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        while(true){
+            boolean check = false;
+            sb.append(map[R][C] + " ");
+
+            for(int i=0; i<4; i++){
+                int ny = R + dy[i];
+                int nx = C + dx[i];
+
+                if(map[ny][nx] > map[R][C]) {
+                    check = true;
+                    R += dy[i];
+                    C += dx[i];
+                    break;
+                }
+            }
+
+            if(!check) break;
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public static boolean isInrange(int x, int y){
+        return x>=0 && y >= 0 && x < N && y < N;
+    }
+}
