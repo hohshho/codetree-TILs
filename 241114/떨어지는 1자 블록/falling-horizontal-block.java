162a8610f73bc723b45cc1f7883da3e3715aa4c3
hohshho <tkdgur8377@gmail.com>
2024-11-14 14:39:18 +0900

    Title: 떨어지는 1자 블록, Time: 124ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241114/떨어지는 1자 블록/falling-horizontal-block.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M, K;
+    static int[][] map;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        K = sc.nextInt() - 1;
+
+        map = new int[N][N];
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        int line = 0;
+        while(true){
+            // K, K+1, K+2
+            boolean check = true;
+
+            for(int i=K; i<M; i++){
+                if(map[line][i] == 1) check = false;
+            }
+            
+            // 전 라인에 1추가
+            if(!check) {
+                line -= 1;
+                for(int i=K; i<M; i++){
+                    map[line][i] = 1;
+                }
+                break;
+            }
+            else {
+                line += 1;
+            }
+        }
+
+        for(int[] row : map){
+            for(int item : row){
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
